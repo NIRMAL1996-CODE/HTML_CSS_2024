8341962e4a418602f3d08d97e73e82df3b3409e6
nirmal <nirmaljaiswal888@gmail.com>
2024-07-26 20:38:57 +0530

    js for vertical tabs

---
+++ VerticalTABS/v_tab.js
@@ -0,0 +1,13 @@
+// const btn = document.querySelector('button');
+// btn.addEventListener("click",function()
+// {
+//   showContent(btn);
+// }); made by me
+
+
+// function open(cityname) {
+//   let tabcontent = document.getElementById("#London")
+   
+//     tabcontent.style.display = "block";
+// }
+// open(London);
