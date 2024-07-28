a031200ae53b97875e6a701acdce26c78487a134
nirmal <nirmaljaiswal888@gmail.com>
2024-07-28 16:17:45 +0530

    tab js

--- VerticalTABS/v_tab.js
+++ VerticalTABS/v_tab.js
@@ -11,3 +11,20 @@
 //     tabcontent.style.display = "block";
 // }
 // open(London);
+
+ let btn =document.querySelectorAll(".button");
+ let contents=document.querySelectorAll(".content");
+ 
+    for(let i= 0; i<btn.length; i++)
+    {
+        btn[i].addEventListener('click', function (){
+
+            for(let j=0; j< contents.length;j++)
+            {
+                contents[j].classList.remove('active');
+            }
+            
+        document.getElementById(this.getAttribute('data-tab')).classList.add('active'); 
+    });
+
+}
